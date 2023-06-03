daff23137078df01cf8e027d46a417b01db09da6
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2023-06-03 09:03:35 +0900

    Add: TodoList

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import TodoList from "./TodoList";
+
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
+    </>
   );
 }
 
---
+++ src/TodoList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// 関数コンポーネント
+const TodoList = () => {
+  return (
+    <div>TodoList</div>
+  )
+}
+
+export default TodoList
